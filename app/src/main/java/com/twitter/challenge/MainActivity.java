2d0da57bdbe656238261c21ddbe199ca2f3aaf9b
Sid Gowda <sidgowda.9@gmail.com>
2021-08-03 18:10:51 -0400

    Converted to androidX

--- app/src/main/java/com/twitter/challenge/MainActivity.java
+++ app/src/main/java/com/twitter/challenge/MainActivity.java
@@ -1,8 +1,8 @@
 package com.twitter.challenge;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
